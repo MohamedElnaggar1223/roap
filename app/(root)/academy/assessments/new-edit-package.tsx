0e62fce7eeb8e1febab8fe9c97da0fb4f6644276
MohamedElnaggar1223 <maelnaggar1223@gmail.com>
2025-01-11 22:22:12 +0200

    Assessment

--- app/(root)/academy/assessments/new-edit-package.tsx
+++ app/(root)/academy/assessments/new-edit-package.tsx
@@ -205,8 +205,8 @@ export default function EditPackage({ packageEdited, open, onOpenChange, mutate,
                 const result = await updatePackage(packageEdited.id, {
                     name: packageName!,
                     price: parseFloat(values.price),
-                    startDate: format(values.startDate, 'yyyy-MM-dd'),
-                    endDate: format(values.endDate, 'yyyy-MM-dd'),
+                    startDate: values.startDate.toLocaleString(),
+                    endDate: values.endDate.toLocaleString(),
                     schedules: values.schedules.map(schedule => ({
                         id: (schedule as any).id,
                         day: schedule.day,
@@ -220,9 +220,9 @@ export default function EditPackage({ packageEdited, open, onOpenChange, mutate,
                     entryFeesAppliedUntil: values.type === "Monthly" && showEntryFeesFields ?
                         values.entryFeesAppliedUntil : undefined,
                     entryFeesStartDate: values.type !== "Monthly" && showEntryFeesFields ?
-                        format(values.entryFeesStartDate!, 'yyyy-MM-dd') : undefined,
+                        values.entryFeesStartDate?.toLocaleString() : undefined,
                     entryFeesEndDate: values.type !== "Monthly" && showEntryFeesFields ?
-                        format(values.entryFeesEndDate!, 'yyyy-MM-dd') : undefined,
+                        values.entryFeesEndDate?.toLocaleString() : undefined,
                     capacity: 9999,
                     type: values.type
                 })
